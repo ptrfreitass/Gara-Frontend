5fdbc091f8cb5318d0bb004530fb31873b8070fa
ptrfreitass <ptrfreitass@gmail.com>
2025-12-19 00:21:30 -0300

    chore: Ajusta config inicial Angular, docker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+
+import { app_config } from './core/config/app-config';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: app_config, useValue: app_config }
   ]
 };
---
+++ src/app/core/config/app-config.ts
@@ -0,0 +1,7 @@
+export interface AppConfig {
+  apiUrl: string;
+}
+
+export const app_config: AppConfig = {
+  apiUrl: 'http://localhost:8000/api/v1', // dev
+};

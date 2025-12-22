452dea2e58df0fa4b3df00c46295252bd2910e4b
ptrfreitass <ptrfreitass@gmail.com>
2025-12-21 23:03:49 -0300

    chore: Landing page e ajustes inicias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/components/layout-component/layout-component';
+import { Landing } from './features/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Página de recepção
+    {
+        path: '',
+        component: Landing
+        //canActivate: [noAuthGuard]
+    },
+];
+
--- src/app/core/config/app-config.ts
+++ src/app/core/config/app-config.ts
@@ -3,5 +3,8 @@ export interface AppConfig {
 }
 
 export const app_config: AppConfig = {
+  ///production: false,
   apiUrl: 'http://localhost:8000/api/v1', // dev
+  //version: '1.0.0',
+  //appName: 'Gara'
 };
